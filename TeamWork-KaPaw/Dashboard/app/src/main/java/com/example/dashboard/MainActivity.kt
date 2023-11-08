8c8471b62d0dfd9d6325d1af37d466cd6109ca52
Jheron <jdv1104@student.uc-bcf.edu.ph>
2023-11-08 22:58:26 +0800

    intent

--- TeamWork-KaPaw/Dashboard/app/src/main/java/com/example/dashboard/MainActivity.kt
+++ TeamWork-KaPaw/Dashboard/app/src/main/java/com/example/dashboard/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.dashboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clickListener();
+
+    }
+    public fun clickListener(){
+        var imageFaqs = findViewById<CardView>(R.id.faqCard)
+        var imageAdoptions = findViewById<CardView>(R.id.adoptionsCard)
+
+        imageFaqs.setOnClickListener{
+            openFaqs()
+        }
+        imageAdoptions.setOnClickListener{
+            openAdoptions()
+
+        }
+    }
+    public fun openFaqs(){
+        startActivity(Intent(this@MainActivity, Faqs::class.java))
+    }
+    public fun openAdoptions(){
+        startActivity(Intent(this@MainActivity, Adoptions::class.java))
     }
 }
