45e38a3f130e2064b15374ab6de6a4b945949e62
Roxan Lagto <roxanlagto001@gmail.com>
2023-12-06 16:31:38 +0800

    Update

---
+++ TeamWork-KaPaw/Login_main/app/src/main/java/com/example/loginpage/Ikue_Checklist.kt
@@ -0,0 +1,11 @@
+package com.example.loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ikue_Checklist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ikue_checklist)
+    }
+}
